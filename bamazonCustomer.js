ca5d6f0ddc7232106727c9c18027ad24c6dd6fa0
tesh-bhatia <nbhatia03@gmail.com>
2018-02-16 22:59:17 -0500

    Connects to database, lists all items

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,40 @@
 require("dotenv").config();
 
+
 var password = require('./password').database.password,
     inquirer = require('inquirer'),
-    mysql = require('mysql');
+    mysql = require('mysql'),
+    Table = require('cli-table'),
+    colors = require('colors')
 
+var connection = mysql.createConnection({
+    host: '127.0.0.1',
+    port: 3306,
+    user: 'root',
+    password: password,
+    database: 'bamazon'
+})
+
+connection.connect(function(err){
+    if(err) throw err;
+    console.log('Connected! ID #' + connection.threadId)
+    readItems()
+})
+
+function readItems () {
+    connection.query("SELECT * FROM products", function(err, res){
+        if(err) throw err;
+        console.log(res)
+        var table = new Table({
+            head : ['Item ID', 'Product Name', 'Department Name', 'Price', 'Stock'],
+            colWidths: [10, 40, 30, 10, 10]
+        })
+        res.forEach(function(obj){
+            var itemInfo = [obj.item_id, obj.product_name, obj.department_name, obj.price, obj.stock_quantity]
+
+            table.push(itemInfo)
+        })
+
+        console.log(table.toString())
+    })
+}
