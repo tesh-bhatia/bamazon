3e138a61f8c039e9fb513b61f57b5fc2b7dfb4f3
tesh-bhatia <nbhatia03@gmail.com>
2018-02-21 18:35:51 -0500

    All manager menu options work

--- bamazonManager.js
+++ bamazonManager.js
@@ -18,11 +18,12 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err){
     if(err) throw err;
-    console.log('Connected! ID #' + connection.threadId)
+    // console.log('Connected! ID #' + connection.threadId)
     
 })
 
 function showMenu () {
+    updateItems()
     inquirer.prompt([
         {
             message: 'Please select a menu option',
@@ -40,8 +41,10 @@ function showMenu () {
                 readLowInventory()
                 break;
             case 'Add to Inventory' :
+                selectItem()
                 break;
             case 'Add New Product' :
+                tryToAddItem()
                 break;
         }
     })
@@ -60,7 +63,7 @@ function readItems () {
             var itemInfo = [obj.item_id, obj.product_name, obj.department_name, obj.price, obj.stock_quantity]
 
             //put items into global array
-            items.push(obj.product_name)
+            items.push(obj.product_name.toLowerCase().trim())
             
             table.push(itemInfo)
         })
@@ -81,9 +84,6 @@ function readLowInventory () {
         if(res.length > 0){
             res.forEach(function(obj){
                 var itemInfo = [obj.item_id, obj.product_name, obj.department_name, obj.price, obj.stock_quantity]
-    
-                //put items into global array
-                items.push(obj.product_name)
                 
                 table.push(itemInfo)
             })
@@ -94,6 +94,104 @@ function readLowInventory () {
         }
 
     })
+}
+
+function selectItem () {
+    inquirer.prompt([
+        {
+            message: "Which product's inventory should change?",
+            name: 'product'
+        },{
+            message: "What is the new inventory?",
+            name: 'stock'
+        }
+    ]).then(function(answer){
+        var item = answer.product.toLowerCase().trim()
+        
+        if(items.includes(item)){
+            addInventory(item, answer.stock)
+        }else{
+            console.log('Please select a valid item')
+            selectItem()
+        }
+    })
+}
+
+function addInventory (item, stock) {
+    connection.query(
+        'UPDATE products SET ? WHERE ?',
+        [
+            {
+                stock_quantity: stock
+            },
+            {
+                product_name: item
+            }
+        ],
+        function(err, res){
+            if(err) throw err
+            console.log(item + ' inventory changed to ' + stock)
+        })
+}
+
+function tryToAddItem () {
+    inquirer.prompt([
+        {
+            name: 'item',
+            message: 'What item would you like to add?'
+        },
+        {
+            name: 'stock',
+            message: 'What is the quantity of this item?'
+        },
+        {
+            name: 'dept',
+            message: 'What is the name of the department for this item?'
+        },
+        {
+            name: 'price',
+            message: 'How much does this item cost?'
+        }
+    ]).then(function(answers){
+        //item already exists
+        if(items.includes(answers.item.toLowerCase().trim())){
+            console.log('The item already exists!')
+        }else{
+            addItem(answers)
+        }   
+    })
+}
+
+function addItem (iteminfo){
+    var item = {
+        product_name: iteminfo.item,
+        department_name: iteminfo.dept,
+        price: iteminfo.price,
+        stock_quantity: iteminfo.stock
+    }
+    connection.query(
+        'INSERT INTO products SET ?',
+        item,
+        function(err, res){
+            if(err) throw err
+            console.log(iteminfo.item + ' added!')
+        }
+    )
+}
+
+//grab all item names from DB, push them to array for reference
+function updateItems () {
+    // reset the items array
+    items =[]
+    connection.query('SELECT product_name FROM products', function (err, res){
+        if(err) throw err
+
+        res.forEach(function(obj){
+            items.push(obj.product_name.toLowerCase())
+        })
+
+    })
+
 }
 
 showMenu()
